509c507f3d22186cf30b7aca2ca9af55ede46779
seredganyha <karix05@yandex.ru>
2024-09-17 14:03:04 +0300

    update Providers SettingsModule

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -2,32 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsService } from './settings.service';
 import { SettingsComponent } from './settings.component';
-import { ThemeService } from '../core/services/theme.service';
-import { CURRENT_THEME } from '../core/tokens/theme-token';
-import { ReactiveFormsModule } from '@angular/forms';
-import { THEMES } from '../core/tokens/themes-token';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SelectComponent } from '../shared/ui/select/select.component';
+import { CURRENT_THEME_PROVIDERS, THEMES_PROVIDERS } from '../core/providers/theme.provider';
 
 
 @NgModule({
   declarations: [SettingsComponent],
   imports: [
     CommonModule,
-    ReactiveFormsModule,
     SelectComponent
   ],
   providers: [ 
-    SettingsService,     
-    {
-      provide: CURRENT_THEME,
-      useFactory: (themeService: ThemeService) => themeService.currentTheme$,
-      deps: [ThemeService]
-    },
-    {
-      provide: THEMES,
-      useFactory: (themeService: ThemeService) => themeService.themes$,
-      deps: [ThemeService]
-    } 
+    SettingsService,
+    CURRENT_THEME_PROVIDERS,
+    THEMES_PROVIDERS,
   ],
   exports: [SettingsComponent]
 })
