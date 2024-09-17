abdc1f309f7b1505513d7d1085061f1a483954ab
seredganyha <karix05@yandex.ru>
2024-09-17 14:06:31 +0300

    add APP_INITIALIZER | theme init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SettingsModule } from './settings/settings.module';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { ThemeService } from './core/services/theme.service';
 
 
 @NgModule({
@@ -16,7 +17,14 @@ import { BrowserModule } from '@angular/platform-browser';
     SettingsModule, 
     CommonModule,
   ],
-  providers: [],
+  providers: [
+    { 
+      provide: APP_INITIALIZER,
+      useFactory: (themeService: ThemeService) => themeService.init,
+      deps: [ThemeService], 
+      multi: true 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
