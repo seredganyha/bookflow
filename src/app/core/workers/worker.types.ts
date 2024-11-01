8a4868ea45b5d643fcb4379d692e9efc59b3e03b
seredganyha <karix05@yandex.ru>
2024-11-01 12:52:50 +0300

    refactoring: worker types
    add readonly
    enum -> const enum

--- src/app/core/workers/worker.types.ts
+++ src/app/core/workers/worker.types.ts
@@ -1,15 +1,28 @@
 export interface WorkerMessage {
-  command: string;
-  worker: string;
-  requestId: string;
+  readonly command: string;
 }
 
-export interface WorkerRequest<T = any> extends WorkerMessage {
-  payload: T;
+export interface InternalMessage<T = unknown> extends WorkerMessage  {
+  readonly requestId: string;
+  readonly payload: T;
+  readonly worker: string;
 }
 
-export interface WorkerResponse<T = any> extends WorkerMessage {
-  payload: T;
+export interface WorkerRequest<T = unknown> extends WorkerMessage {
+  readonly payload: T;
+}
+
+export interface WorkerResponse<T = unknown> extends WorkerMessage {
+  readonly payload: T;
 }
 
 export type Constructor = new (...args: any[]) => {};
+
+export const enum Workers {
+  BookWorker = "BookWorker",
+}
+
+export interface WorkerDefinition {
+  readonly name: string;
+  readonly workerClass: Worker;
+}
